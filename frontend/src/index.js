e5af38911a673913175cfa0ca100b8172c74e0bc
rohit bhardwaj <rb8329563@gmail.com>
2024-12-15 22:44:27 +0530

    changed the ui and updated the home page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 import { BrowserRouter, Routes, Route } from "react-router";
 import reportWebVitals from './reportWebVitals';
 import Semester from './pages/Semester';
 import Subject from './pages/Subject';
 import Note from './pages/Note';
 import Result from './pages/Result';
+import Home from './pages/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <BrowserRouter>
@@ -16,6 +18,7 @@ root.render(
   <Route path='/main' element={<App/>}/>
   <Route path='/note' element={<Note/>}/>
   <Route path='/result' element={<Result/>}/>
+  <Route path='/home' element={<Home/>}/>
 </Routes>
 </BrowserRouter>
 );
